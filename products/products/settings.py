9112a2ce5643694cb87489b28d53c48a0d6675a7
PhilippMatthes <github@philippmatth.es>
2020-01-15 16:48:25 +0100

    Change default service locations

--- products/products/settings.py
+++ products/products/settings.py
@@ -28,8 +28,8 @@ DEBUG = bool(int(os.environ.get('DEBUG', default=1)))
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", default="*").split()
 
-VERIFICATION_SERVICE_URL = os.environ.get("VERIFICATION_SERVICE_URL", default="http://127.0.0.1:8000")
-LOCATIONS_SERVICE_URL = os.environ.get("LOCATIONS_SERVICE_URL", default="http://127.0.0.1:8001")
+VERIFICATION_SERVICE_URL = os.environ.get("VERIFICATION_SERVICE_URL", default="http://verification:8000")
+LOCATIONS_SERVICE_URL = os.environ.get("LOCATIONS_SERVICE_URL", default="http://locations:8000")
 
 
 # Application definition
